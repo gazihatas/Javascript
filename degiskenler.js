6a39cb6e88cb2ee5f94106d8e344cdd09bc8c8db
gazihatas <55117177+gazihatas@users.noreply.github.com>
2021-02-26 01:46:56 +0300

    Update degiskenler.js
    javascript tutorial

---
+++ degiskenler.js
@@ -0,0 +1,28 @@
+// var, let ve const ile değişken tanımlanabilir
+//noktalı virgül kullanımı isteğe bağlıdır
+//değişken isimleri bazı kurallara göre verilir
+
+var yasim = 31;
+console.log(yasim);
+console.log(yasim);
+console.log(yasim);
+console.log(yasim);
+console.log(yasim);
+console.log(yasim);
+
+
+let tcKimlik = 123456;
+let sayi = 123456;
+let sayi222 = 123456;
+let benimDogumYilim = 1998;
+let benim_dogum_yilim = 1998;
+let benim_dogumYilim = 1998;
+let sayi1;
+sayi1 = 50;
+sayi1 = 60;
+console.log(sayi1);
+
+
+const piSayisi = 3.14;
+console.log(piSayisi);
+
