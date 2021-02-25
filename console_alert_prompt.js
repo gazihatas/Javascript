ef07847a9e38663ea88394ad0e08fc67a6e0f59c
gazihatas <55117177+gazihatas@users.noreply.github.com>
2021-02-26 01:56:15 +0300

    Enter

---
+++ console_alert_prompt.js
@@ -0,0 +1,21 @@
+let myAge= 22;
+console.log(myAge);
+console.error('Yanlıs islem');
+console.warn('Yetkisiz islem');
+console.clear();
+
+
+console.table({ad : "Ali", yas: 40});
+
+console.time("id");
+console.log("bbb");
+console.log("bbb");
+console.log("bbb");
+console.log("bbb");
+console.log("bbb");
+console.log("bbb");
+console.log("bbb");
+console.log("bbb");
+console.log("bbb");
+console.log("bbb");
+console.timeEnd("id");
