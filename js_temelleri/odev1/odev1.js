77c3d7f576f2ab36519811151f098d7ce9ef31e3
gazihatas <55117177+gazihatas@users.noreply.github.com>
2021-02-26 16:46:16 +0300

    Enter

---
+++ js_temelleri/odev1/odev1.js
@@ -0,0 +1,10 @@
+/* Odev 1
+Bir değişken oluşturun ve bu değişkende saniye değerini tutun.
+Sonra bu saniye değerinin kaç dakika ve saniyeye denk geldiğini yazdırın.
+Ornek 1200 değeri için 20 dakika 0 saniye yazmalıdır.
+*/
+
+let saniye = 60;
+let dakika = 20;
+dakika = saniye * dakika;
+console.log(dakika + " : " + saniye );
