5805b5de4cef5e2309967b54f549aa84795c8b07
gazihatas <55117177+gazihatas@users.noreply.github.com>
2021-02-26 12:27:24 +0300

    Enter

---
+++ js_temelleri/operatorler.js
@@ -0,0 +1,78 @@
+//Aritmatik operatorşer + - * / %
+let sayi1 = 10;
+let sayi2 =  3;
+
+/*
+let sonuc =  sayi1 + sayi2;
+let sonuc =  sayi1 - sayi2;
+let sonuc =  sayi1 * sayi2;
+let sonuc =  sayi1 / sayi2;
+
+sonuc = sayi1 % sayi2;
+console.log(sonuc);
+
+let isim = 'Ahmet';
+let yas = 25;
+let metinOlarakYas = 25;
+let soyIsim = 'Emre';
+let tamIsim = isim + soyIsim;
+console.log(tamIsim);
+
+
+consolee.log(metinOlarakYas + 100);
+console.log(metinOlarakYas * 10);
+*/
+
+//Arttırma azaltma Operatörleri
+/*
+sayi1 = sayi1 + 1;
+sayi1++;
+++sayi1;
+sayi1--;// sayi1 = sayi1 - 1
+--sayi1;
+console.log(sayi1);
+
+console.log("Sayi 2 :" + sayi2);
+console.log("Sayi 2 :" + sayi2++);
+console.log("Sayi 2 :" +sayi2);
+
+let sayi3 = 20;
+console.log(++sayi3);
+console.log(sayi3);
+*/
+
+//Aritmatik atama operatörleri
+// +=, -=, *=, /=, %=
+let sayi4 = 10;
+let sayi5 = 4;
+//sayi2 += sayi1; // sayi2 = sayi2 + sayi1;
+//sayi2 -= sayi1; // sayi2 = sayi2 - sayi1;
+console.log(sayi2);
+
+
+console.log(sayi1 > sayi2);
+console.log(sayi1 < sayi2);
+console.log(sayi1 <= sayi2);
+console.log(sayi1 >= sayi2);
+console.log(sayi1 != sayi2);
+console.log(sayi1 == sayi2);
+
+let yas = 30;
+let metinselYas = '30';
+console.log(yas  == metinselYas);//değerler karşılaştırılır
+console.log(yas === metinselYas);// hem değere hem de veri tipine bakar
+console.log(yas !== metinselYas);//hem değere hem de veri tipie bakar
+
+//mantıksal operatörler ve, veya
+
+console.log( (sayi1 > 5) && (sayi2 < 10) );
+console.log( (sayi1 < 5 ) || (sayi2 < 10) );
+
+let sonuc = 4 + 10 / 2 - 1 * 5;
+let sonuc2 = (4 + 10) / ((2 - 1) * 5);
+// 4 + 10 / 2 - 5
+//4 + 5 - 5
+//9 - 5
+// 4
+console.log(sonuc);
+
